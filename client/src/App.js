26ef9bdbe90425def15aae427527a3ee0aabc95e
RyanMontoya <ryan.montoya@comcast.net>
2022-05-28 23:03:31 -0700

    implementing react with new DB

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,39 @@
-import React,{useState, useEffect} from "react"; 
+//import React,{useState, useEffect} from "react";  
+import React,{useState} from "react"; 
 import './App.css';
 import Axios from 'axios';
 function App() { 
 
  const [spellName,setSpellName]= useState('') 
  const [spellLevel,setSpellLevel]= useState('')
+ const [school,setSpellSchool] = useState('')  
+ const [ritual,setRitual] = useState('')  
+ const [castingtime,setCastingTime] = useState('')  
+ const [components,setComponents] = useState('')  
+ const [duration,setDuration] = useState('')  
+ const [spellrange,setSpellRange] = useState('')  
+ const [concentration,setConcentration] = useState('')  
+ const [booksource,setBookSource] = useState('')  
+ const [spelltext,setSpellText] = useState('')  
+ const [spelltype,setSpellType] = useState('') 
 
  const submitSpell = () => { 
  Axios.post('http://localhost:3001/api/insert',{ 
    spellName:spellName, 
-   spellLevel:spellLevel,  
-  }). then(()=> { 
+   spellLevel:spellLevel, 
+   school:school, 
+   ritual:ritual, 
+   castingtime:castingtime, 
+   components:components, 
+   duration:duration, 
+   spellrange:spellrange, 
+   concentration:concentration, 
+   booksource:booksource, 
+   spelltext:spelltext, 
+   spelltype:spelltype 
+
+
+  }).then(()=> { 
     alert("successful insert")
   })
  };
@@ -26,7 +49,47 @@ function App() {
     <label> spell level</label> 
     <input type="text" name= "spell level"onChange={(e)=> { 
      setSpellLevel(e.target.value)
+    }}/>   
+       <label> spell school</label> 
+    <input type="text" name= "spell school"onChange={(e)=> { 
+     setSpellSchool(e.target.value)
     }}/>  
+    <label> ritual</label> 
+    <input type="text" name= "spell ritual casting"onChange={(e)=> { 
+     setRitual(e.target.value)
+    }}/>   
+    <label> spell casting time </label> 
+    <input type="text" name= "spell casting time"onChange={(e)=> { 
+     setCastingTime(e.target.value)
+    }}/>  
+    <label> spell components</label> 
+    <input type="text" name= "spell components"onChange={(e)=> { 
+     setComponents(e.target.value)
+    }}/>  
+     <label> spell duration</label> 
+    <input type="text" name= "spell duration"onChange={(e)=> { 
+     setDuration(e.target.value)
+    }}/>  
+     <label> spell range</label> 
+    <input type="text" name= "spell range"onChange={(e)=> { 
+     setSpellRange(e.target.value)
+    }}/>  
+     <label> concentration</label> 
+    <input type="text" name= "concentration"onChange={(e)=> { 
+     setConcentration(e.target.value)
+    }}/>  
+     <label> book source</label> 
+    <input type="text" name= "booksource"onChange={(e)=> { 
+     setBookSource(e.target.value)
+    }}/>  
+     <label> spell text</label> 
+    <input type="text" name= "spell text"onChange={(e)=> { 
+     setSpellText(e.target.value)
+    }}/>  
+     <label> spell type</label> 
+    <input type="text" name= "spell type"onChange={(e)=> { 
+     setSpellType(e.target.value)
+    }}/> 
     <button onClick={submitSpell}>submit</button>
     </div>
   </div>  
--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const  db =mysql.createPool({
     host:'localhost', 
     user: 'root' ,
     password: 'beans0987654321$$', 
-    database: 'spellorganizer' 
+    database: 'dnd_db' 
 });
 app.use(cors()); 
 app.use(express.json());
@@ -17,15 +17,27 @@ app.post("/api/insert",(req,res)=> {
     
 
     const spellName = req.body.spellName 
-    const spellLevel = req.body.spellLevel 
+    const spellLevel = req.body.spellLevel  
+    const school = req.body.school 
+    const ritual = req.body.ritual 
+    const castingtime = req.body.castingtime 
+    const components = req.body.components 
+    const duration = req.body.duration 
+    const spellrange = req.body.spellrange 
+    const concentration = req.body.concentration 
+    const booksource =  req.body.booksource 
+    const spelltext = req.body.spelltext 
+    const spelltype = req.body.spelltype 
 
-    const sqlInsert = "insert into allspellstable (spellname,spelllevel,school,ritual,castingtime,components,duration,concentration,booksource,spelltext) values (?,?,'bean','bean','bean','bean','bean','bean','bean','bean')"
-    db.query(sqlInsert,[spellName,spellLevel], (err,result)=> { 
+   // const sqlInsert = "insert into spells (sp_no,spellName,spelllevel,school,ritual,castingtime,components,duration,spellrange,concentration,book_no,booksource _spelltext_spelltype_us_no) values (11,'test',5,'test','no')"
+   // const sqlInsert = "insert into allspellstable (spellname,spelllevel,school,ritual,castingtime,components,duration,concentration,booksource,spelltext) values (?,?,'bean','bean','bean','bean','bean','bean','bean','bean')" 
+   const sqlInsert = "INSERT INTO spells (sp_no,spellName,spelllevel,school,ritual,castingtime,components,duration,spellrange,concentration,book_no,booksource,spelltext,spelltype,us_no) VALUES (0,?,?,?,?,?,?,?,?,?,1,?,?,?,1);"
+    db.query(sqlInsert,[spellName,spellLevel,school,ritual,castingtime,components,duration,spellrange,concentration,booksource,spelltext,spelltype], (err,result)=> { 
        console.log(err);
     });
 
 });
-
+/*
 app.get("/", (req,res)  => {    
      
 const sqlInsert = "INSERT INTO allspellstable (spellname,spelllevel,school,ritual,castingtime,components,duration,concentration,booksource,spelltext) VALUES ('faerie fire','1','evocation','false', '1 action','V','concentration 1 minute','true','phb', 'beans222');"; 
@@ -34,7 +46,7 @@ const sqlInsert = "INSERT INTO allspellstable (spellname,spelllevel,school,ritua
     res.send("hello pedro12345678011232223445678910101");
  });
 
-}); 
+}); */
 
 app.listen(3001, () => { 
 console.log("running on  port 3001");
